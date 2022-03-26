ed7b598c387a765949db7c9d264acbc48c8c6301
Douglas Silva Santos <douglas@seguroviagem.srv.br>
2022-03-26 11:06:52 -0300

    Feito a Migration

--- priv/repo/migrations/20210415171125_create_meals_table.exs
+++ priv/repo/migrations/20210415171125_create_meals_table.exs
@@ -2,6 +2,10 @@ defmodule Exmeal.Repo.Migrations.CreateMealsTable do
   use Ecto.Migration
 
   def change do
-   # TO DO
+    create table(:meals) do
+      add :descricao, :string
+      add :data, :utc_datetime
+      add :caloria, :integer
+    end
   end
 end
